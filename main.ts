66f4d9794722a83b9a6ca1a18038842fb0d71c78
simon-bursa <146720121+simon-bursa@users.noreply.github.com>
2024-02-22 20:05:50 +0100

    Uděláno znova, ztratil jsem původní verzi

--- main.ts
+++ main.ts
@@ -1,29 +1,32 @@
-/*
-    cas: je doba v sekundách, po kterou trouba
-    odebírala uvedený příkon
-    spotreba: je kolik energie by bylo spotřebováno,
-    pokud by takto trouba pekla celou hodinu
-    (jsou to watthodiny [Wh])
 
-    pro cas: 600s a spotreba: 3500 Wh je skutečné
-    odebrané množství "energie":
-    3500 * (600 / 3600) === 3500 * (1/6) === 583,333W
-*/
 type Odber = {
-    cas: number, //čas je v sekundách
-    spotreba: number //spotřeba ve watech za hodinu
-}
-const cena: number = 6.70; // Kč / kWh
+    cas: number,
+    spotreba: number 
+};
+
 let data: Array<Odber> = [
     { cas: 600, spotreba: 3500 },
     { cas: 120, spotreba: 0 },
-    { cas: 300, spotreba: 1700 }, // data[2].cas
+    { cas: 300, spotreba: 1700 }, 
     { cas: 60, spotreba: 0 },
     { cas: 800, spotreba: 1500 },
-]
+];
 
-for (const peceme of data){
-    console.logValue("čas: ", peceme.cas);
-    console.logValue("spotřeba: ", peceme.spotreba);
-    console.logValue("spotřebováno: ", peceme.spotreba / 3600 * peceme.cas);
+let prumer: number = 0;
+const cena: number = 6.70; 
+
+for (let i = 0; i < data.length; i++) {
+    let cenaFinal = Math.round(data[i].spotreba / 1000 * (data[i].cas / 3600) * cena);
+    prumer += cenaFinal;
+
+    console.log(
+        "Spotřeba: " + data[i].spotreba + " W" +
+        "\nČas: " + data[i].cas + " s" +
+        "\nCena za kWh: " + cena + " Kč" +
+        "\nCelková cena: " + cenaFinal + " Kč" +
+        "\n---"
+    );
 }
+
+console.log("Průměrná cena: " + prumer / data.length + " Kč")
+
